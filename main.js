f3597ea6ce585017e980828a37eba9a48a0d6d46
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-03-19 21:18:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
-    
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Bo3VjkaNu/model.json",modelLoaded)
 }
 function preload(){
 
@@ -11,3 +11,19 @@ function preload(){
 function draw(){
     image(video,0,0,300,250)
 }
+function modelLoaded(){
+    console.log("im out");
+    }
+    function draw(){
+        image(video,0,0,300,250)
+        classifier.classify(video,gotResults);
+    }
+    function gotResults(error,result){
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(result);
+            document.getElementById("magufin").innerHTML=result[0].label;
+            document.getElementById("aim").innerHTML=result[0].confidence.toFixed(2)*100+"%";
+        }
+    }
